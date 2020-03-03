25f2ac5f6c06080608c10ec3163f9a1af9db2c8f
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-03-03 15:17:18 +0100

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('contact', 'ContactController@getForm');
+Route::post('contact', 'ContactController@postForm');
+Route::get('change-password', 'ChangerPasswordController@index');
+Route::view('palteforme', 'home');
+Route::view('compte','compte');
+Route::post('change-password', 'ChangerPasswordController@store')->name('change.password');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
