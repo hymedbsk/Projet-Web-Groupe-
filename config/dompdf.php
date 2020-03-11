1bd71d8df8d5e06fe785fa0f54c9434cbce8e163
Melvin Campos Casares <melvin.campos@outlook.be>
2020-03-11 09:52:26 +0100

    First stage of configuration for domPDF

---
+++ config/dompdf.php
@@ -0,0 +1,16 @@
+// config/dompdf.php
+
+<?php
+
+return [
+
+    'chroot' => realpath(base_path()),
+    'fontDir' => storage_path('fonts/'),
+    'fontCache' => storage_path('fonts/'),
+    'defaultMediaType' => 'screen',
+    'defaultPaperSize' => 'a4',
+    'dpi' => 96,
+    'defaultFont' => 'serif',
+    'fontHeightRatio' => 1.1,
+
+];
