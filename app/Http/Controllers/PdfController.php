42fd4ca5be87ab9ee99a14cceacfd87678698611
Melvin Campos Casares <melvin.campos@outlook.be>
2020-03-14 16:43:56 +0100

    First stage of correct invoice

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -14,7 +14,17 @@ class PdfController extends Controller
      */
     public function generatePDF()
     {
-        $data = ['title' => 'Facture'];
+        $data = ['title' => 'Facture',
+          'date' => date("d/m/Y"),
+          'providerName' => 'EPHEC ASBL',
+          'providerRoad' => 'Avenue Konrad Adenauer 3',
+          'providerLocality' => '1200 Woluwe-Saint-Lambert',
+          'providerCountry' => 'Belgique',
+          'clientName' => 'Toto',
+          'clientRoad' => 'Avenue du Ciseau 15',
+          'clientLocality' => '1348 Louvain-la-Neuve',
+          'clientCountry' => 'Belgique',
+          'totalInvoice' => 1000];
         $pdf = PDF::loadView('myPDF', $data);
   
         return $pdf->download('invoice.pdf');
--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -2,14 +2,50 @@
 <html>
 <head>
 	<title>PDF</title>
+	<style>
+	body {
+		margin-top: 1em 0;
+	}
+	.entreprise, .total { text-align: right; }
+	</style>
 </head>
 <body>
-	<h1>EPHEC Entreprendre - {{ $title }}</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<h1>EPHEC Entreprendre</h1>
+	<div class="entreprise">
+		<p><em><strong>Prestataire :</strong></em>
+		<br>{{ $providerName }}
+		<br>{{ $providerRoad }}
+		<br>{{ $providerLocality }}
+		<br>{{ $providerCountry }}</p>
+	</div>
+	<div class="client">
+		<p><em><strong>Client :</strong></em>
+		<br>{{ $clientName }}
+		<br>{{ $clientRoad }}
+		<br>{{ $clientLocality }}
+		<br>{{ $clientCountry }}</p>
+	</div>
+	<p><strong>{{ $title }}</strong></p>
+	<p>Date de la facture : {{ $date }}</p>
+	<hr>
+	<div class="details">
+		<h2>Détails de la facture :</h2>
+		<table style="width:100%">
+		<tr>
+			<th>Description</th>
+			<th>Quantité</th>
+			<th>Prix unitaire HTVA</th>
+			<th>Taux TVA</th>
+			<th>Prix unitaire TTC</th>
+			<th>Total TTC</th>
+		</tr>
+		<!-- <tr>
+			<td></td>
+		</tr> -->
+		</table>
+	</div>
+	<div class="total">
+		<p><strong>Total facturé :</strong> {{ $totalInvoice }} €</p>
+	</div>
 </body>
 </html>
