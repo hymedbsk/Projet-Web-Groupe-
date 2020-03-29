00da1e3c617b54e977619e13199bfc04a3167eab
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-03-29 16:18:21 +0200

    Add files via upload

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      */
 
     protected $fillable = [
-        'matricule','nom', 'prenom', 'email', 'password',
+        'matricule','nom', 'prenom', 'email', 'password', 'Type'
     ];
 
     /**
