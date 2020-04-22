ebd1d53ade42e1e69f8fce0fcf80e030598d7500
Ash <antoinehoe@gmail.com>
2020-04-22 09:35:31 +0200

    info event

---
+++ app/event.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class event extends Model
+{
+    protected $table = 'event';
+    protected $primaryKey = "id_Activite";
+    protected $fillable = ['id_Activite', 'Local', 'Date', 'Theme', 'Organisteur'];
+    public $timestamps = false;
+
+    public function users(){
+       
+        return $this->belongsToMany('App\User', 'userbyevent', 'id_Activite', 'id');    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,24 @@ Route::post('contact', 'ContactController@postForm');
 Route::view('palteforme', 'home');
 Route::view('compte','compte');
 Auth::routes();
-Route::get('/profil', 'ChangerPasswordController@index');
-Route::post('/profil', 'ChangerPasswordController@store')->name('change.password');
-Route::get('/home', 'homeController@index');
-Route::resource('post', 'PostController');
-Route::resource('user', 'UserController');
-Route::get('MesAnnonces', 'PostController@show');
+Route::get('/test', 'testcontroler@do'); //test
+Route::get('/user', 'UserController@index');
+Route::view('/error','error');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/testNotif', 'testControlleurNotif@main');
+
+Route::get('/testPivotTable','EventController@index'); //test
+Route::get('/testboutonselect/{event}', 'EventController@selectEvent')->name('testboutonselect'); //test
+
+Route::get('/listEtu','eventController@listUserToEvent')->name('listUserByEvent'); //real
+
+Route::get('/addUserToEvent/{user}', 'EventController@addUserToEvent')->name('addUserToEvent');
+Route::get('/removeUserFromEvent/{user}', 'EventController@removeUserFromEvent')->name('removeUserFromEvent');
+
+Route::get('/selectEvent/{user}', 'EventController@selectEvent')->name('selectEvent');
+
+// vue générale event
+Route::get('/tableEvent', 'EventController@listEvent');
+    // info event
+Route::get('/infoEvent/{event}', 'EventController@infoEvent')->name('infoEvent');
