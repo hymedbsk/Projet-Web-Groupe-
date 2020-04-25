29c20d477f250dc97eb6a2507ad5d56251d1d592
Ash <antoinehoe@gmail.com>
2020-04-25 18:07:06 +0200

    Validation etudiant + update vue
    Vue permettant la validation/suppression des étudiants encore non approuvés par l'administrateur
    
    Update de la vue générale (etudiants / events / etudiants par event)

--- resources/views/listEtu.blade.php
+++ resources/views/listEtu.blade.php
@@ -1,42 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <br>
-    <div class="row">
-        <div class="col-6">
-            @if(session()->has('ok'))
-                <div class="alert alert-success alert-dismissible">{!! session('ok') !!}</div>
-            @endif
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Liste des évènements</h3>
-                </div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Theme</th>
-                            <th>Professeur</th>
-                            <th>Date</th>
-                            <th>Local</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                        @foreach ($events as $event)
-                            <tr>
-                                <td>{!! $event->id_Activite !!}</td>
-                                <td class="text-primary"><strong>{!! $event->Theme !!}</strong></td>
-                                <td class="text-primary"><strong>{!! $event->Organisateur !!}</strong></td>
-                                <td class="text-primary"><strong>{!! $event->Date !!}</strong></td>
-                                <td class="text-primary"><strong>{!! $event->Local !!}</strong></td>
-                                <td> <a class="btn btn-primary" href="{{route('selectEvent', $event)}}" role="button">Sélectionner</a></td>
-                            </tr>
-                        @endforeach                    
-                    </tbody>
-                </table>
-            </div>
-            <div class="panel panel-primary">
+<div class="panel panel-primary">
                 <div class="panel-heading">
                     <h3 class="panel-title">Liste des utilisateurs</h3>
                 </div>
@@ -52,55 +17,18 @@
                     <tbody>
                         
                         @foreach ($users as $user)
-                            @if ($user->accountChecked==1)
+                            @if ($user->accountChecked==0)
                             <tr>
                                 <td>{!! $user->id !!}</td>
                                 <td class="text-primary"><strong>{!! $user->name !!}</strong></td>
                                 <td class="text-primary"><strong>{!! $user->email !!}</strong></td>
                                 <td class="text-primary"><strong>{!! $user->accountChecked !!}</strong></td>
-                                <td> <a class="btn btn-primary" href="{{route('addUserToEvent', $user)}}" role="button">Ajouter</a></td>
+                                <td> <a class="btn btn-primary btn-success" href="{{route('valUser', $user)}}" role="button">Valider</a></td>
+                                <td> <a class="btn btn-primary btn-danger" href="{{route('delUser', $user)}}" role="button">Supprimer</a></td>
                             </tr>
                             @endif
                         @endforeach                    
                     </tbody>
                 </table>
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class=" col-6">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Liste des participants</h3>
-                </div>
-                
-                <tbody>
-                    @foreach ($events as $event)
-                        <br>
-                        <h4 class="panel-title">{!! $event->Theme!!}</h4>
-                        <table class="table">
-                            <thead>
-                                <th>ID event</th>
-                                <th>{!! $event->Theme!!}</th>
-                                <th>ID Etudiant</th>
-                                <th>Nom</th>
-                            </thead>
-
-                        @foreach ($event->users as $user)
-
-                            <tr>
-                                <th>{!! $event->id_Activite!!}</th>
-                                <th>{!! $event->Theme!!}</th>
-                                <td>{!! $user->id!!}</td>
-                                <td class="text-primary"><strong>{!! $user->name!!}</strong></td>
-                                <td> <a class="btn btn-primary" href="{{route('removeUserFromEvent', $user)}}" role="button">Retirer</a></td>
-                            </tr>
-                        
-                        @endforeach 
-                        </table>
-                    @endforeach                     
-                </tbody>
-            </div>
-        </div>
-    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,29 @@ Route::get('/user', 'UserController@index');
 Route::view('/error','error');
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('formPdf', 'FormPdfController@index');
-Route::post('genPdf', 'PDFMaker@gen');
-
 Route::get('/testNotif', 'testControlleurNotif@main');
 
 Route::get('/testPivotTable','EventController@index'); //test
 Route::get('/testboutonselect/{event}', 'EventController@selectEvent')->name('testboutonselect'); //test
 
-Route::get('/listEtu','eventController@listUserToEvent')->name('listUserByEvent'); //real
+Route::get('/globalView','eventController@globalView')->name('globalView'); //real
+// validation etudiant
+Route::get('/listEtu', 'UserController@list')->name('list');
 
-Route::get('/addUserToEvent/{user}', 'EventController@addUserToEvent')->name('addUserToEvent');
-Route::get('/removeUserFromEvent/{user}', 'EventController@removeUserFromEvent')->name('removeUserFromEvent');
+Route::get('/delUser/{user}', 'UserController@deleteInvalidUser')->name('delUser');
 
-Route::get('/selectEvent/{user}', 'EventController@selectEvent')->name('selectEvent');
+Route::get('/valUser/{user}', 'UserController@validUser')->name('valUser');
+
 
 // vue générale event
 Route::get('/tableEvent', 'EventController@listEvent');
     // info event
 Route::get('/infoEvent/{event}', 'EventController@infoEvent')->name('infoEvent');
+
+Route::get('/manageEvent/{event}', 'EventController@manageEvent')->name('manageEvent');
+
+Route::get('/addUserToEvent/{user}', 'EventController@addUserToEvent')->name('addUserToEvent');
+
+Route::get('/removeUserFromEvent/{user}', 'EventController@removeUserFromEvent')->name('removeUserFromEvent');
+
+Route::get('/selectEvent/{user}', 'EventController@selectEvent')->name('selectEvent');
