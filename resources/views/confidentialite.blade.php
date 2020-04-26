790f302b9e262f7928873dc1e6bb65331cdb3f3f
Melvin Campos Casares <melvin.campos@outlook.be>
2020-04-26 16:50:48 +0200

    Tweaks on 'Politique de confidentialité'
    * Updated design
    * Add footer

--- resources/views/confidentialite.blade.php
+++ resources/views/confidentialite.blade.php
@@ -1,118 +1,163 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="page-section">
+<header class="masthead">
   <div class="container">
-    <div class="row">
-      <div class="col-lg-12 text-center">
-        <h2 class="section-heading text-uppercase">Politique de confidentialité</h2>
+    <div class="intro-text">
+      <div class="card shadow-lg p-3 mb-5 bg-white rounded">
+        <div class="card-body" >
+          <blockquote class="blockquote mb-0 font-italic">
+            <p>Politique de confidentialité</p>
+          </blockquote>
+        </div>
       </div>
     </div>
-    <p><strong>Introduction</strong>
-      <br>Devant le développement des nouveaux outils de communication, il est nécessaire de porter une attention particulière à la protection de la vie privée. 
-      C'est pourquoi, nous nous engageons à respecter la confidentialité des renseignements personnels que nous collectons.
-    </p>
-    <hr>
-
-    <h3>Collecte des renseignements personnels</h3>
-
-    <p>Nous collectons les renseignements suivants : </p>
-    <ul>
-      <li>Nom</li>
-      <li>Prénom</li>
-      <li>Adresse électronique</li>
-      <li>Scolarité / Formation</li>
-    </ul>
-    <p>Les renseignements personnels que nous collectons sont recueillis au travers de formulaires et grâce à  l'interactivité établie entre vous et notre site Web.
-      Nous utilisons également, comme indiqué dans la section suivante, des fichiers témoins et/ou journaux pour réunir des informations vous concernant.</p>
-    <hr>
-
-    <h3>Formulaires et interactivité :</h3>
-
-    <p>Vos renseignements personnels sont collectés par le biais de formulaire, à savoir :</p>
-    <ul>
-      <li>Formulaire d'inscription au site Web</li>
-    </ul>
-    <p>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</p>
-    <ul>
-      <li>Contact</li>
-      <li>Gestion du site Web (présentation, organisation)</li>
-      <li>Publication d'annonce sur la plateforme</li>
-    </ul>
-    <p>Vos renseignements sont également collectés par le biais de l'interactivité pouvant s'établir entre vous et notre site Web et ce, de la façon suivante :</p>	
-    <ul>
-      <li>Contact</li>
-      <li>Gestion du site Web (présentation, organisation)</li>
-      <li>Publication d'annonce sur la plateforme</li>
-    </ul>
-    <p>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :<br /></p>
-    <ul>
-      <li>Forum ou formulaire de discussion</li>
-      <li>Informations</li>
-    </ul>
-    <hr>
-
-    <h3>Fichiers journaux et témoins </h3>
- 
-    <p>Nous recueillons certaines informations par le biais de fichiers journaux (log file) et de fichiers témoins (cookies). Il s'agit principalement des informations suivantes :</p>
-    <ul>
-      <li>Adresse IP</li>
-      <li>Pages visitées et requêtes</li>
-      <li>Heure et jour de connexion</li>
-    </ul>
-    <p>Le recours à de tels fichiers nous permet : </p>
-    <ul>
-      <li>Amélioration du service et accueil personnalisé</li>
-    </ul>
-    <hr>
-
-    <h3>Droit d'opposition et de retrait</h3>
-
-    <p>Nous nous engageons à vous offrir un droit d'opposition et de retrait quant à vos renseignements personnels.
-      <br>Le droit d'opposition s'entend comme étant la possiblité offerte aux internautes de refuser que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.</p>
-    <p>Le droit de retrait s'entend comme étant la possiblité offerte aux internautes de demander à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.</p>
-    <p>Pour pouvoir exercer ces droits, vous pouvez :</p>
-      <ul>
-        <li>Code postal : Avenue Konrad Adenauer 3, 1200 Woluwe-Saint-Lambert, Belgique</li>
-        <li>Courriel : <a href="mailto:ephec-entreprendre@ephec.be">ephec-entreprendre@ephec.be</a></li>
-        <li>Section du site web : <a href="entreprendre.che2-ephec.be/contact" target="_blank">entreprendre.che2-ephec.be/contact</a></li>
-      </ul>
-    <hr>
-
-    <h3>Droit d'accès</h3>
-
-    <p>Nous nous engageons à reconnaître un droit d'accès et de rectification aux personnes concernées désireuses de consulter, modifier, voire radier les informations les concernant.</p>
-      <p>L'exercice de ce droit se fera :</p>
-      <ul>
-        <li>Code postal : Avenue Konrad Adenauer 3, 1200 Woluwe-Saint-Lambert, Belgique</li>
-        <li>Courriel : <a href="mailto:ephec-entreprendre@ephec.be">ephec-entreprendre@ephec.be</a></li>
-        <li>Section du site web : <a href="entreprendre.che2-ephec.be/contact" target="_blank">entreprendre.che2-ephec.be/contact</a></li>
-      </ul>
-    <hr>
-
-    <h2>Sécurité</h2>
-
-    <p>Les renseignements personnels que nous collectons sont conservés dans un environnement sécurisé.
-      <br>Les personnes travaillant pour nous sont tenues de respecter la confidentialité de vos informations.
-      <br>Pour assurer la sécurité de vos renseignements personnels, nous avons recours aux mesures suivantes :</p>
-    <ul>
-      <li>Protocole SSL (Secure Sockets Layer)</li>
-      <li>Gestion des accès - personne autorisée</li>
-      <li>Gestion des accès - personne concernée</li>
-      <li>Identifiant / mot de passe</li>
-      <li>Pare-feu (Firewalls)</li>
-    </ul>
-    <p>Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières innovations technologiques permettant d'assurer la confidentialité de vos transactions.
-      <br>Toutefois, comme aucun mécanisme n'offre une sécurité maximale, une part de risque est toujours présente lorsque l'on utilise Internet pour transmettre des renseignements personnels.</p>
-    <hr>
-
-    <h3>Législation</h3>
-
-    <p>Nous nous engageons à respecter les dispositions législatives énoncées dans :
-      <br><a href="https://www.ephec.be/mentions-legales" target="_blank">https://www.ephec.be/mentions-legales</a>
-    </p>
   </div>
-</section>
+</header>
+
+<div class="container">
+  <p><strong>Introduction</strong>
+    <br>Devant le développement des nouveaux outils de communication, il est nécessaire de porter une attention particulière à la protection de la vie privée. 
+    C'est pourquoi, nous nous engageons à respecter la confidentialité des renseignements personnels que nous collectons.</p>
+  <hr>
+
+  <h3>Collecte des renseignements personnels</h3>
+
+  <p>Nous collectons les renseignements suivants : </p>
+  <ul>
+    <li>Nom</li>
+    <li>Prénom</li>
+    <li>Adresse électronique</li>
+    <li>Scolarité / Formation</li>
+  </ul>
+  <p>Les renseignements personnels que nous collectons sont recueillis au travers de formulaires et grâce à  l'interactivité établie entre vous et notre site Web.
+    Nous utilisons également, comme indiqué dans la section suivante, des fichiers témoins et/ou journaux pour réunir des informations vous concernant.</p>
+  <hr>
+
+  <h3>Formulaires et interactivité :</h3>
+
+  <p>Vos renseignements personnels sont collectés par le biais de formulaire, à savoir :</p>
+  <ul>
+    <li>Formulaire d'inscription au site Web</li>
+  </ul>
+  <p>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</p>
+  <ul>
+    <li>Contact</li>
+    <li>Gestion du site Web (présentation, organisation)</li>
+    <li>Publication d'annonce sur la plateforme</li>
+  </ul>
+  <p>Vos renseignements sont également collectés par le biais de l'interactivité pouvant s'établir entre vous et notre site Web et ce, de la façon suivante :</p>	
+  <ul>
+    <li>Contact</li>
+    <li>Gestion du site Web (présentation, organisation)</li>
+    <li>Publication d'annonce sur la plateforme</li>
+  </ul>
+  <p>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :<br /></p>
+  <ul>
+    <li>Forum ou formulaire de discussion</li>
+    <li>Informations</li>
+  </ul>
+  <hr>
+
+  <h3>Fichiers journaux et témoins </h3>
+ 
+  <p>Nous recueillons certaines informations par le biais de fichiers journaux (log file) et de fichiers témoins (cookies). Il s'agit principalement des informations suivantes :</p>
+  <ul>
+    <li>Adresse IP</li>
+    <li>Pages visitées et requêtes</li>
+    <li>Heure et jour de connexion</li>
+  </ul>
+  <p>Le recours à de tels fichiers nous permet : </p>
+  <ul>
+    <li>Amélioration du service et accueil personnalisé</li>
+  </ul>
+  <hr>
+
+  <h3>Droit d'opposition et de retrait</h3>
+
+  <p>Nous nous engageons à vous offrir un droit d'opposition et de retrait quant à vos renseignements personnels.
+    <br>Le droit d'opposition s'entend comme étant la possiblité offerte aux internautes de refuser que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.</p>
+  <p>Le droit de retrait s'entend comme étant la possiblité offerte aux internautes de demander à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.</p>
+  <p>Pour pouvoir exercer ces droits, vous pouvez :</p>
+  <ul>
+    <li>Code postal : Avenue Konrad Adenauer 3, 1200 Woluwe-Saint-Lambert, Belgique</li>
+    <li>Courriel : <a href="mailto:ephec-entreprendre@ephec.be">ephec-entreprendre@ephec.be</a></li>
+    <li>Section du site web : <a href="entreprendre.che2-ephec.be/contact" target="_blank">entreprendre.che2-ephec.be/contact</a></li>
+  </ul>
+  <hr>
+
+  <h3>Droit d'accès</h3>
+
+  <p>Nous nous engageons à reconnaître un droit d'accès et de rectification aux personnes concernées désireuses de consulter, modifier, voire radier les informations les concernant.</p>
+  <p>L'exercice de ce droit se fera :</p>
+  <ul>
+    <li>Code postal : Avenue Konrad Adenauer 3, 1200 Woluwe-Saint-Lambert, Belgique</li>
+    <li>Courriel : <a href="mailto:ephec-entreprendre@ephec.be">ephec-entreprendre@ephec.be</a></li>
+    <li>Section du site web : <a href="entreprendre.che2-ephec.be/contact" target="_blank">entreprendre.che2-ephec.be/contact</a></li>
+  </ul>
+  <hr>
+
+  <h2>Sécurité</h2>
+
+  <p>Les renseignements personnels que nous collectons sont conservés dans un environnement sécurisé.
+    <br>Les personnes travaillant pour nous sont tenues de respecter la confidentialité de vos informations.
+    <br>Pour assurer la sécurité de vos renseignements personnels, nous avons recours aux mesures suivantes :</p>
+  <ul>
+    <li>Protocole SSL (Secure Sockets Layer)</li>
+    <li>Gestion des accès - personne autorisée</li>
+    <li>Gestion des accès - personne concernée</li>
+    <li>Identifiant / mot de passe</li>
+    <li>Pare-feu (Firewalls)</li>
+  </ul>
+  <p>Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières innovations technologiques permettant d'assurer la confidentialité de vos transactions.
+    <br>Toutefois, comme aucun mécanisme n'offre une sécurité maximale, une part de risque est toujours présente lorsque l'on utilise Internet pour transmettre des renseignements personnels.</p>
+  <hr>
+
+  <h3>Législation</h3>
+
+  <p>Nous nous engageons à respecter les dispositions législatives énoncées dans :
+    <br><a href="https://www.ephec.be/mentions-legales" target="_blank">https://www.ephec.be/mentions-legales</a>
+  </p>
+</div>
+
+<footer class="footer">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-md-4">
+        <span class="copyright">Copyright &copy; Ephec entreprendre 2019-2020</span>
+      </div>
+      <div class="col-md-4">
+        <ul class="list-inline social-buttons">
+          <li class="list-inline-item">
+          <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+          </li>
+          <li class="list-inline-item">
+          <a href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          </li>
+          <li class="list-inline-item">
+          <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <ul class="list-inline quicklinks">
+          <li class="list-inline-item">
+            <a href="confidentialite">Politique de confidentialité</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="cgu">Condition d'utilisation</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
 @endsection
 
 
