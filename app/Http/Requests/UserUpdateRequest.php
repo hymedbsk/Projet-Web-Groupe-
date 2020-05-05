cdcb258f7088704fdc14070938c4dfb70d53a5fb
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-05-05 14:59:28 +0200

    Add files via upload

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -14,11 +14,9 @@ class UserUpdateRequest extends FormRequest
 
 	public function rules()
 	{
-		$id = $this->user->id;
+	
         return [
-            'name' => 'required|max:255|unique:users,name,' . $id,
-			'email' => 'required|email|max:255|unique:users,email,' . $id
-        ];
-
+            
+	];
         }
 }
