19ac988789524aa980217f7c34b5eb5e39670334
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-05-06 00:37:41 +0200

    Add files via upload

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,63 +3,102 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\UserRepository;
-
+use App\Http\Requests\UserUpdateRequest;
+use App\Http\Requests\UserCreateRequest;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use App\User;
-
 class UserController extends Controller
 {
 
     protected $userRepository;
 
-    protected $nbrPerPage = 10;
+    protected $nbrPerPage = 4;
 
     public function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
+	    {
+		$this->middleware('prof'); 
+		$this->userRepository = $userRepository;
+		/*$this->middleware('prof');*/
+	}
 
-    public function index()
-    {
-        $users = $this->userRepository->getPaginate($this->nbrPerPage);
-        $links = $users->render();
+	public function index()
+	{
+		$users = $this->userRepository->getPaginate($this->nbrPerPage);
+		$links = $users->render();
 
-        return view('user', compact('users', 'links'));
-    }
-    public function show($id)
-    {
-        $user = $this->userRepository->getById($id);
+		return view('user.UserList', compact('users', 'links'));
+	}
 
-        return view('show',  compact('user'));
-    }
-    private function setAdmin($request)
+	public function create()
+	{
+		return view('create');
+	}
+
+	public function store(UserCreateRequest $request)
+	{
+		$user = $this->userRepository->store($request->all());
+
+		return redirect('user.UserList')->withOk("L'utilisateur " . $user->name . " a été créé.");
+	}
+
+	public function show($id)
+	{
+		$user = $this->userRepository->getById($id);
+
+		return view('show',  compact('user'));
+	}
+
+	public function edit($id)
+	{
+		$user = $this->userRepository->getById($id);
+
+		return view('user.edit',  compact('user'));
+	}
+
+	public function update(UserUpdateRequest $request, $id)
+	{
+		  $user = User::findOrFail($id);
+		  $user->matricule = $request->input('matricule');
+       		  $user->nom = $request->input('nom');
+        	  $user->prenom = $request->input('prenom');
+	 	  $user->email = $request->input('email');
+        	  $user->save();
+	
+		 return redirect('user');
+		   	
+
+	}
+	public function statut($id){
+
+	 $user = User::findOrFail($id);
+	 
+         if($user->type == 'EN'){
+            User::where('id','=', $user->id)->update(['type'=> 'PR']);
+            Session::flash('message','Utilisateur mis        jour');
+             return redirect('user');
+            }
+            else if($user->type == 'PR'){
+             User::where('id','=', $user->id)->update(['type'=> 'EN']);
+            Session::flash('message', 'Utilisateur mise        jour');
+        return redirect('user');
+            }else{
+
+                return redirect('user');
+            }
+	}
+
+	public function destroy($id)
+	{
+		$this->userRepository->destroy($id);
+
+		return back();
+	}
+   /* private function Admin($request)
     {
         if(!$request->has('admin'))
         {
             $request->merge(['admin' => 0]);
-        }   
-    }   
-    
-    public function list(){
-       
-        $users = User::get();
-        
-        return view('/listEtu', compact('users'));
-    }
-
-    public function deleteInvalidUser(User $user){
-        $userToDelete = $user;
-        
-        User::where('id','=', $userToDelete->id)->delete();
-
-        return redirect()->route('list');
-    }
-
-    public function validUser(User $user){
-        $userToValid = $user;
-        
-        User::where('id','=', $userToValid->id)->update(['accountChecked'=> 1]);
-
-        return redirect()->route('list');
-    }
+        }
+    }*/
 }
